e3cd99f798e6063337507c48f98de5a1500a06ec
Vishal <vishalkumartcd@gmail.com>
2020-12-15 17:10:33 +0000

    SingelNumber solution using XOR operator

---
+++ 30Days/Week1/SingleNumber.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3283/
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+public class SingleNumber{
+    
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2,4,6};
+
+        Solution sol = new Solution();
+        int result = sol.singleNumber(nums);
+
+        System.out.println(result);
+
+    }
+
+}
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i=1;i<nums.length; i++) {
+            nums[0] ^= nums[i]; 
+        }
+        
+        return nums[0];        
+    }
+}
